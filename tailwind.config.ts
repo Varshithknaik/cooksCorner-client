f75118f40d7e7192f8125c44d4b6338bcc7853b3
Varshith K <varshithknaik@gmail.com>
2024-02-26 21:57:35 +0530

    initial code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        Poppins: ["var(--font-Poppins)"],
+        Josefin: ["var(--font-Josefin)"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        "1000px":"1000px",
+        "1100px":"1100px",
+        "1200px":"1200px",
+        "1300px":"1300px",
+        "1500px":"1500px",
+        "800px":"800px",
+        "400px":"400px",
+      },
+      colors: {
+        primary: 'var(--clr-primary)',
+        secondary: 'var(--clr-secondary)',
+        text: 'var(--clr-text)',
+        'text-faded': 'var(--clr-text-faded)',
+        'input-background': 'var(--clr-input-background)',
+        background: 'var(--clr-background)',
       },
     },
   },
